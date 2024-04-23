76491808c54674ebad5b8c8af62015295529a061
Tim Davis <contact@timdav.is>
2024-04-22 19:04:17 -0700

    switch to authentik

--- services/postgres/docker-entrypoint-initdb.d/create-databases.sh
+++ services/postgres/docker-entrypoint-initdb.d/create-databases.sh
@@ -6,7 +6,7 @@ create_databases() {
   [ -z "$POSTGRES_DATABASES" ] && return
   for db in $POSTGRES_DATABASES; do
     psql -U "postgres" -c "create user $db with password '$db'";
-    psql -U "postgres" -c "create database $db";
+    psql -U "postgres" -c "create database $db owner $db";
     psql -U "postgres" -c "grant all privileges on database $db to $db";
   done
 }
